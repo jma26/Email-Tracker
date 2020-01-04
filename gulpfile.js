9de5c4da042c492794a5647481b9c2453c0a134c
Jesse Ma <j.masonoma17@gmail.com>
2020-01-04 04:30:51 -0800

    BrowserSync now works with css injection

--- gulpfile.js
+++ gulpfile.js
@@ -1,62 +1,26 @@
 var gulp = require('gulp');
     uglify = require('gulp-uglify');
     sass = require('gulp-sass');
-    sass.compiler = require('node-sass');
-    connect = require('gulp-connect');
-
-gulp.task('server', function() {
-  connect.server({
-    root: 'build',
-    port: 8000,
-    livereload: true,
-  })
-})
-
-gulp.task('html', function(done) {
-  gulp.src('frontend/index.html')
-      .pipe(gulp.dest('build'))
-      .on('end', function() {
-        done();
-      })
-      .pipe(connect.reload());
-})
-
-gulp.task('js', function(done) {
-  gulp.src('frontend/*.js')
-      .pipe(uglify().on('error', function(error) {
-        // Handle any js errors
-        done(error);
-      }))
-      .pipe(gulp.dest('build'))
-      .on('end', function() {
-        done();
-      })
-      .pipe(connect.reload());
+    browserSync = require('browser-sync').create();
+    
+// Compile scss into css and auto inject into browsers
+gulp.task('scss', function() {
+  return gulp.src('frontend/styles/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('frontend/styles'))
+    .pipe(browserSync.reload({ stream: true }))
 });
 
-gulp.task('scss', function(done) {
-  gulp.src('frontend/*.scss')
-      .pipe(sass.sync().on('error', function(error) {
-        done(error);
-      }))
-      .pipe(gulp.dest('build'))
-      .on('end', function() {
-        done();
-      })
-      .pipe(connect.reload());
-});
+gulp.task('serve', gulp.series('scss', function() {
+  browserSync.init({
+    server: {
+      baseDir: 'frontend',
+      index: 'index.html'
+    }
+  });
+  gulp.watch('frontend/*.html').on('change', browserSync.reload);
+  gulp.watch('frontend/js/*.js').on('change', browserSync.reload);
+  gulp.watch('frontend/styles/*.scss', gulp.series('scss'));
+}));
 
-gulp.task('default', gulp.series('html', 'js', 'scss', 'server'), function(done) {
-  gulp.watch(['frontend/*.html'], ['html']);
-  gulp.watch(['frontend/*.scss'], ['scss']);
-  gulp.watch(['frontend/*.js'], ['js']);
-  done();
-});
-
-function done(error) {
-  if (error) {
-    console.log('Build error:', error);
-  } else {
-    console.log('Build successful');
-  }
-}
+gulp.task('default', gulp.parallel('serve'));
