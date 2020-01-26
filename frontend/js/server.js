c67656509ca1da00c9b74e0ebcfd1fd367782bd7
Jesse Ma <j.masonoma17@gmail.com>
2020-01-25 22:41:23 -0800

    Add mobile styling to landing page

--- frontend/js/server.js
+++ frontend/js/server.js
@@ -1,1 +1,4 @@
-console.log(process.env.ApiKey, process.env.clientId);
+const CLIENT_ID = process.env.client_id;
+const API_KEY = process.env.api_key;
+const DISCOVERY_DOCS = ["https://sheets.googleapis.com/$discovery/rest?version=v4"];
+const SCOPES = "https://www.googleapis.com/auth/spreadsheets.readonly";
