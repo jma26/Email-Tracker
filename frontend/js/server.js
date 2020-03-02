4c87cf810f583f4027adaf68db6c531222f5877a
Jesse Ma <j.masonoma17@gmail.com>
2020-03-01 23:43:09 -0800

    Add gulp-live-server package + brainstorming comments

--- frontend/js/server.js
+++ frontend/js/server.js
@@ -1,4 +1,14 @@
-const CLIENT_ID = process.env.client_id;
-const API_KEY = process.env.api_key;
+// const CLIENT_ID = PROCESS.ENV.client_id;
+// const API_KEY = PROCESS.ENV.api_key;
 const DISCOVERY_DOCS = ["https://sheets.googleapis.com/$discovery/rest?version=v4"];
 const SCOPES = "https://www.googleapis.com/auth/spreadsheets.readonly";
+
+var authorizeButton = document.getElementById('authorize_button');
+var signoutButton = document.getElementById('signout_button');
+
+// Fetch API to write to Google Sheets API with oAuth but with API key?
+// https://stackoverflow.com/questions/51081462/using-google-sheets-api-without-client-side-login
+// Do I need a NodeJS server?
+// Is a lightweight server properly setup?
+// Look into gulp-live-server
+// It doesn't seem like a server is setup...at all *face palm*
--- gulpfile.js
+++ gulpfile.js
@@ -2,29 +2,36 @@ var gulp = require('gulp');
     uglify = require('gulp-uglify');
     sass = require('gulp-sass');
     browserSync = require('browser-sync').create();
-    nodemon = require('gulp-nodemon');
-    env = require('gulp-env');
+    dotenv = require('gulp-dotenv'); 
+    concat = require('gulp-concat');
+    babel = require('gulp-babel');
     
 // Compile scss into css and auto inject into browsers
 gulp.task('scss', function() {
   return gulp.src('frontend/styles/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('frontend/styles'))
+    .pipe(gulp.dest('frontend/dist'))
     .pipe(browserSync.reload({ stream: true }))
 });
 
-gulp.task('nodemon', function() {
-  env({
-    file: '.env.json'
-  })
-
-  nodemon({
-    script: 'frontend/js/server.js',
-    ext: 'js'
-  })
+gulp.task('server', function() {
+  return gulp.src('frontend/js/*.js')
+    .pipe(concat('script.js'))
+    .pipe(babel({
+      presets: ['@babel/env']
+    }))
+    .pipe(uglify())
+    .pipe(gulp.dest('frontend/dist'))
+    .pipe(browserSync.reload({ stream: true }))
 })
 
-gulp.task('serve', gulp.series('scss', function() {
+// gulp.task('dotenv', function() {
+//   return gulp.src('.env.json')
+//     .pipe(dotenv())
+//     .pipe(gulp.dest('frontend/dist'))
+// })
+
+gulp.task('dev', gulp.series('scss', function() {
   browserSync.init({
     server: {
       baseDir: 'frontend',
@@ -33,6 +40,7 @@ gulp.task('serve', gulp.series('scss', function() {
   });
   gulp.watch('frontend/*.html').on('change', browserSync.reload);
   gulp.watch('frontend/styles/*.scss', gulp.series('scss'));
+  gulp.watch('frontend/js/*.js', gulp.series('server'));
 }));
 
-gulp.task('default', gulp.parallel('serve', 'nodemon'));
+gulp.task('default', gulp.parallel('dev', 'server'));
